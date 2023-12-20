fbfe111e966640b0df788df0d184cc5e631c2f21
Petchou <86736499+PetchouDev@users.noreply.github.com>
2023-12-20 23:48:19 +0100

    Update du système de mail
    - ne pas poster les creds sur github (lol, je vais devoir les changer)
    - Meilleur affichage
    
    - Besion de tarvailler le message envoyé au format MIME (html)

--- contrib/mail.py
+++ contrib/mail.py
@@ -1,6 +1,8 @@
 import os
 import pathlib
 import smtplib
+import json
+
 # send html or plaintext message
 from email.mime.text import MIMEText
 # send attachments
@@ -12,19 +14,20 @@ message  = MIMEMultipart()
 path = pathlib.Path(os.path.abspath(os.path.dirname(__file__)))
 
 Body = open(path /  "New message.html", "r").read()
+creds = json.load(open(path / "mail_creds.json", "r"))
 
 def send_mail(dest, subject="Notification"):
     dest = message["To"] = dest
-    sender = "inshack@insecurity-insa.fr" #"inshack@insecurity-insa.fr"
+    sender = creds.get("mail") 
     # set sender to a alphanumerical string instead of an email address
     message["From"] = f"INS'Hack 2024 <{sender}>"
-    message["Subject"] = f"INS'HACK 2024 | {subject}]"
+    message["Subject"] = f"INS'HACK 2024 | {subject}"
 
     message.attach(MIMEText(Body, "html"))
 
     server = smtplib.SMTP_SSL("smtppro.zoho.eu", 465)
 
-    server.login(sender, "Ins'hack_1s_Fun!") #"Ins'hack_1s_Fun!"
+    server.login(sender, creds.get("password")) #"Ins'hack_1s_Fun!"
 
     server.sendmail(sender, dest, message.as_string())
 
